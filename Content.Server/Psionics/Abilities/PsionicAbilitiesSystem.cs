cda101b19824a2b482f40863cafc11e533179552
ShatteredSwords <135023515+ShatteredSwords@users.noreply.github.com>
2024-08-15 04:51:33 -0400

    Revert "Update PsionicAbilitiesSystem.cs"
    This reverts commit 7c7ab3f240a96e5633273168481d1158de67d6ef.

--- Content.Server/Psionics/Abilities/PsionicAbilitiesSystem.cs
+++ Content.Server/Psionics/Abilities/PsionicAbilitiesSystem.cs
@@ -7,7 +7,6 @@ using Content.Shared.StatusEffect;
 using Robust.Shared.Random;
 using Robust.Shared.Prototypes;
 using Content.Shared.Popups;
-using Content.Server.MassMedia.Components;
 
 namespace Content.Server.Psionics.Abilities
 {
@@ -38,35 +37,25 @@ namespace Content.Server.Psionics.Abilities
             if (Deleted(uid))
                 return;
 
-            var rollCount = 0;
-            while (rollCount < 3)
-            {
-                if (AddRandomPsionicPower(uid))
-                    break;
-
-                rollCount++;
-            }
-
+            AddRandomPsionicPower(uid);
         }
-        public bool AddRandomPsionicPower(EntityUid uid)
+        public void AddRandomPsionicPower(EntityUid uid)
         {
             EnsureComp<PsionicComponent>(uid, out var psionic);
 
             if (!_prototypeManager.TryIndex<WeightedRandomPrototype>("RandomPsionicPowerPool", out var pool))
             {
                 Logger.Error("Can't index the random psionic power pool!");
-                return false;
+                return;
             }
 
-            var newComponent = _componentFactory.GetComponent(pool.Pick());
+            // uh oh, stinky!
+            var newComponent = (Component) _componentFactory.GetComponent(pool.Pick());
+            newComponent.Owner = uid;
 
-            if (!EntityManager.HasComponent(uid, newComponent.GetType()))
-            {
-                EntityManager.AddComponent(uid, newComponent);
-                _glimmerSystem.DeltaGlimmerInput(_random.NextFloat(psionic.Amplification * psionic.Dampening, psionic.Amplification * psionic.Dampening * 5));
-                return true;
-            }
-            return false;
+            EntityManager.AddComponent(uid, newComponent);
+
+            _glimmerSystem.DeltaGlimmerInput(_random.NextFloat(psionic.Amplification * psionic.Dampening, psionic.Amplification * psionic.Dampening * 5));
         }
 
         public void RemovePsionics(EntityUid uid)
@@ -104,7 +93,7 @@ namespace Content.Server.Psionics.Abilities
 
             _statusEffectsSystem.TryAddStatusEffect(uid, "Stutter", TimeSpan.FromMinutes(5), false, "StutteringAccent");
 
-            _glimmerSystem.DeltaGlimmerOutput(-_random.NextFloat(psionic.Amplification * psionic.Dampening * 5, psionic.Amplification * psionic.Dampening * 10));
+            _glimmerSystem.DeltaGlimmerOutput(-_random.NextFloat((int) MathF.Round(psionic.Amplification * psionic.Dampening * 5), (int) MathF.Round(psionic.Amplification * psionic.Dampening * 10)));
             RemComp<PsionicComponent>(uid);
             RemComp<PotentialPsionicComponent>(uid);
         }
